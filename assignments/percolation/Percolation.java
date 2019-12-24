bce2b47b8da49cb06a8954314aef0fab0f0162bd
CoderYihaoWang <yesiamyihao@gmail.com>
2019-12-25 00:17:03 +1300

    add project

---
+++ assignments/percolation/Percolation.java
@@ -0,0 +1,44 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+/******************************************************************************
+ *  Name:              Yihao Wang
+ *  Last modified:     12/24/2019
+ ******************************************************************************/
+
+public class Percolation {
+    private WeightedQuickUnionUF weightedQuickUnionUF;
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return true;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
