211c2dd8c0a75621c5f4b20e673e42b016f32be7
CoderYihaoWang <yesiamyihao@gmail.com>
2019-12-27 19:28:41 +1300

    optimized with reservoir sampling

--- assignments/queues/Permutation.java
+++ assignments/queues/Permutation.java
@@ -1,19 +1,25 @@
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
 
 public class Permutation {
     public static void main(String[] args) {
         int k = Integer.parseInt(args[0]);
         RandomizedQueue<String> rq = new RandomizedQueue<String>();
-        for (int i = 0; i < k; ++i)
+        int i = 0;
+        for (; i < k; ++i)
             rq.enqueue(StdIn.readString());
+        // reservoir sampling
         while (!StdIn.isEmpty()) {
-            if (!rq.isEmpty()) {
+            ++i;
+            int other = StdRandom.uniform(i);
+            String str = StdIn.readString();
+            if (other < k) {
                 rq.dequeue();
-                rq.enqueue(StdIn.readString());
+                rq.enqueue(str);
             }
         }
-        for (int i = 0; i < k; ++i)
+        for (int j = 0; j < k; ++j)
             StdOut.println(rq.dequeue());
     }
 }
