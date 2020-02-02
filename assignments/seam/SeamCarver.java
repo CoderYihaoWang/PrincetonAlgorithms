e1a840c96bada5adae19c524920d920ea8bc68ae
CoderYihaoWang <yesiamyihao@gmail.com>
2020-02-03 11:52:50 +1300

    completed assignment

--- assignments/seam/SeamCarver.java
+++ assignments/seam/SeamCarver.java
@@ -1,14 +1,14 @@
 
-
-
 /* *****************************************************************************
- *  Name:
- *  Date:
- *  Description:
+ *  Name: SeamCarver.java
+ *  Date: 3/2/2020
+ *  Description: Resizes a picture using the seam carving algorithm
  **************************************************************************** */
 
 import edu.princeton.cs.algs4.Picture;
 import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Stopwatch;
 
 public class SeamCarver {
 
@@ -16,10 +16,14 @@ public class SeamCarver {
     private int height;
     private int width;
     private double[][] energy;
+    // is the picture stored in transposed mode?
     private boolean transposed = false;
 
     // create a seam carver object based on the given picture
     public SeamCarver(Picture picture) {
+        if (picture == null)
+            throw new IllegalArgumentException("The picture cannot be null");
+
         this.picture = new Picture(picture);
         height = picture.height();
         width = picture.width();
@@ -33,38 +37,30 @@ public class SeamCarver {
     private double computeEnergy(int x, int y) {
         if (x == 0 || x == height - 1 || y == 0 || y == width - 1)
             return 1000.0;
-        int c = picture.getRGB(y, x);
         int up = picture.getRGB(y, x - 1);
         int down = picture.getRGB(y, x + 1);
         int left = picture.getRGB(y - 1, x);
         int right = picture.getRGB(y + 1, x);
-        return Math.sqrt(computeDelta(c, up, down) + computeDelta(c, left, right));
+        return Math.sqrt(computeDelta(up, down) + computeDelta(left, right));
     }
 
-    private double computeDelta(int c, int left, int right) {
-        int cr = (c >> 16) & 0xff;
-        int cg = (c >> 8) & 0xff;
-        int cb = c & 0xff;
+    private double computeDelta(int left, int right) {
         int lr = (left >> 16) & 0xff;
         int lg = (left >> 8) & 0xff;
         int lb = left & 0xff;
         int rr = (right >> 16) & 0xff;
         int rg = (right >> 8) & 0xff;
         int rb = right & 0xff;
-        return (cr - lr) * (cr - lr)
-                + (cg - lg) * (cg - lg)
-                + (cb - lb) * (cb - lb)
-                + (cr - rr) * (cr - rr)
-                + (cg - rg) * (cg - rg)
-                + (cb - rb) * (cb - rb);
+        return (rr - lr) * (rr - lr) + (rg - lg) * (rg - lg) + (rb - lb) * (rb - lb);
     }
 
     // current picture
     public Picture picture() {
+        // makes sure that the returned picture is not transposed
         if (transposed)
             transpose();
         transposed = false;
-        return picture;
+        return new Picture(picture);
     }
 
     // width of current picture
@@ -85,6 +81,11 @@ public class SeamCarver {
 
     // energy of pixel at column x and row y
     public double energy(int x, int y) {
+        if (transposed)
+            transpose();
+        transposed = false;
+        if (x >= width || y >= height || x < 0 || y < 0)
+            throw new IllegalArgumentException("Invalid indices!");
         return energy[y][x];
     }
 
@@ -92,9 +93,9 @@ public class SeamCarver {
     public int[] findHorizontalSeam() {
         if (!transposed)
             transpose();
-        transposed = false;
+        transposed = false; // change transposed to false to make it acceptable for findVerticalSeam
         int[] ans = findVerticalSeam();
-        transposed = true;
+        transposed = true; // and then change it back
         return ans;
     }
 
@@ -103,6 +104,7 @@ public class SeamCarver {
         if (transposed)
             transpose();
         transposed = false;
+
         double[][] distTo = new double[height][width];
         int[][] edgeTo = new int[height][width];
         for (int i = 0; i < width; ++i) {
@@ -136,7 +138,6 @@ public class SeamCarver {
                 minId = i;
             }
         }
-
         int[] ans = new int[height];
         Stack<Integer> stack = new Stack<>();
         for (int i = height - 1; i >= 0; --i) {
@@ -163,10 +164,46 @@ public class SeamCarver {
             transpose();
         transposed = false;
 
+        if (width <= 1)
+            throw new IllegalArgumentException("Cannot remove as the width is too small");
+        if (seam == null)
+            throw new IllegalArgumentException("The seam cannot be null!");
+        if (seam.length != height)
+            throw new IllegalArgumentException("Invalid seam!");
+        int last = seam[0];
+        if (last >= width || last < 0)
+            throw new IllegalArgumentException("Invalid seam!");
+        for (int i = 1; i < height; ++i) {
+            if (seam[i] >= width || seam[i] < 0 || Math.abs(seam[i] - last) > 1)
+                throw new IllegalArgumentException("Invalid seam!");
+            last = seam[i];
+        }
 
+        Picture tp = new Picture(width - 1, height);
+        for (int i = 0; i < height; ++i) {
+            for (int j = 0; j < seam[i]; ++j)
+                tp.setRGB(j, i, picture.getRGB(j, i));
+            for (int j = seam[i]; j < width - 1; ++j)
+                tp.setRGB(j, i, picture.getRGB(j + 1, i));
+        }
+        picture = tp;
+        width = width - 1;
+        double[][] te = new double[height][width];
+        for (int i = 0; i < height; ++i) {
+            if (seam[i] != 0) {
+                System.arraycopy(energy[i], 0, te[i], 0, seam[i] - 1);
+                te[i][seam[i] - 1] = computeEnergy(i, seam[i] - 1);
+            }
+            if (seam[i] != width) {
+                te[i][seam[i]] = computeEnergy(i, seam[i]);
+                System.arraycopy(energy[i], seam[i] + 2, te[i], seam[i] + 1, width - 1 - seam[i]);
+            }
+        }
+        energy = te;
     }
 
     private void transpose() {
+        // switching height and width
         int t = height;
         height = width;
         width = t;
@@ -182,9 +219,38 @@ public class SeamCarver {
         energy = te;
     }
 
-    //  unit testing (optional)
+    //  test client copied from ResizeDemo.java
     public static void main(String[] args) {
+        if (args.length != 3) {
+            StdOut.println(
+                    "Usage:\njava ResizeDemo [image filename] [num cols to remove] [num rows to remove]");
+            return;
+        }
 
+        Picture inputImg = new Picture(args[0]);
+        int removeColumns = Integer.parseInt(args[1]);
+        int removeRows = Integer.parseInt(args[2]);
+
+        StdOut.printf("image is %d columns by %d rows\n", inputImg.width(), inputImg.height());
+        SeamCarver sc = new SeamCarver(inputImg);
+
+        Stopwatch sw = new Stopwatch();
+
+        for (int i = 0; i < removeRows; i++) {
+            int[] horizontalSeam = sc.findHorizontalSeam();
+            sc.removeHorizontalSeam(horizontalSeam);
+        }
+
+        for (int i = 0; i < removeColumns; i++) {
+            int[] verticalSeam = sc.findVerticalSeam();
+            sc.removeVerticalSeam(verticalSeam);
+        }
+        Picture outputImg = sc.picture();
+
+        StdOut.printf("new image size is %d columns by %d rows\n", sc.width(), sc.height());
+
+        StdOut.println("Resizing time: " + sw.elapsedTime() + " seconds.");
+        inputImg.show();
+        outputImg.show();
     }
-
 }
